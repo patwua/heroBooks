13195ec8a95d3181207f6af1fa984c56931cc470
patwua <patwuablog@gmail.com>
2025-08-30 00:33:26 -0400

    chore: streamline build and render config

--- src/lib/defaultVatCodes.ts
+++
@@ -1,5 +0,0 @@
-export const DEFAULT_VAT_CODES = [
-  { code: "STANDARD", name: "Standard VAT 20%", rate: 0.2 },
-  { code: "REDUCED", name: "Reduced VAT 5%", rate: 0.05 },
-  { code: "ZERO", name: "Zero VAT 0%", rate: 0 }
-];
