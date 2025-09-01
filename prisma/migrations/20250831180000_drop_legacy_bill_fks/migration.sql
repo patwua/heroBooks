5519294b0c0a26cbe63e571c7e35a2a1f36a4d76
patwua <patwuablog@gmail.com>
2025-09-01 13:32:39 -0400

    chore: clean up bill line migration

--- prisma/migrations/20250831180000_drop_legacy_bill_fks/migration.sql
+++ prisma/migrations/20250831180000_drop_legacy_bill_fks/migration.sql
@@ -2,13 +2,13 @@
 ALTER TABLE "Bill" DROP CONSTRAINT IF EXISTS "Bill_vendorId_fkey";
 ALTER TABLE "BillLine" DROP CONSTRAINT IF EXISTS "BillLine_billId_fkey";
 ALTER TABLE "BillLine" DROP CONSTRAINT IF EXISTS "BillLine_taxCodeId_fkey";
+ALTER TABLE "BillLine" DROP CONSTRAINT IF EXISTS "BillLine_itemId_fkey";
+
+-- Add missing itemId column for composite relation
+ALTER TABLE "BillLine" ADD COLUMN     "itemId" TEXT;
 
 -- Add composite foreign keys
 ALTER TABLE "Bill" ADD CONSTRAINT "Bill_vendorId_orgId_fkey" FOREIGN KEY ("vendorId", "orgId") REFERENCES "Vendor"("id", "orgId") ON DELETE RESTRICT ON UPDATE CASCADE;
 ALTER TABLE "BillLine" ADD CONSTRAINT "BillLine_billId_orgId_fkey" FOREIGN KEY ("billId", "orgId") REFERENCES "Bill"("id", "orgId") ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE "BillLine" ADD CONSTRAINT "BillLine_taxCodeId_orgId_fkey" FOREIGN KEY ("taxCodeId", "orgId") REFERENCES "TaxCode"("id", "orgId") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- Indexes for the new composite keys
-CREATE INDEX IF NOT EXISTS "Bill_vendorId_orgId_idx" ON "Bill"("vendorId", "orgId");
-CREATE INDEX IF NOT EXISTS "BillLine_billId_orgId_idx" ON "BillLine"("billId", "orgId");
-CREATE INDEX IF NOT EXISTS "BillLine_taxCodeId_orgId_idx" ON "BillLine"("taxCodeId", "orgId");
+ALTER TABLE "BillLine" ADD CONSTRAINT "BillLine_taxCodeId_orgId_fkey" FOREIGN KEY ("taxCodeId", "orgId") REFERENCES "TaxCode"("id", "orgId") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "BillLine" ADD CONSTRAINT "BillLine_itemId_orgId_fkey" FOREIGN KEY ("itemId", "orgId") REFERENCES "Item"("id", "orgId") ON DELETE RESTRICT ON UPDATE CASCADE;
