3a8780391d49e1543f6f54be3b16c893c2786af2
patwua <patwuablog@gmail.com>
2025-09-07 16:55:02 -0400

    chore: revert next-env.d.ts

--- src/components/marketing/FloatingTrialCTA.tsx
+++ src/components/marketing/FloatingTrialCTA.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 export default function FloatingTrialCTA() {
   return (
     <div className="fixed bottom-4 right-4 z-50">
-      <Button asChild size="lg">
+      <Button asChild>
         <Link href="/sign-up">Start a free trial</Link>
       </Button>
     </div>
